6c8daa9a2c4475309f295ca0d42453ccf36108ea
CraggyPop <craggles360@gmail.com>
2024-12-23 15:07:28 +0200

    Pink_garnet, raw_pink_garnet, Episode 2

--- src/main/java/co/za/ren/tutorialmod/TutorialMod.java
+++ src/main/java/co/za/ren/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package co.za.ren.tutorialmod;
 
+import co.za.ren.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -20,5 +21,7 @@ public class TutorialMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
